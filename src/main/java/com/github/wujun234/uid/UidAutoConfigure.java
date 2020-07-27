46d73373330c748b57f9449e75cf363b629ae5ae
wangcy <wangcy@ushareit.com>
2020-07-27 23:45:45 +0800

    enable custom Hikari dataSource

--- src/main/java/com/github/wujun234/uid/UidAutoConfigure.java
+++ src/main/java/com/github/wujun234/uid/UidAutoConfigure.java
@@ -1,10 +1,15 @@
 package com.github.wujun234.uid;
 
 import com.github.wujun234.uid.impl.CachedUidGenerator;
+import com.github.wujun234.uid.impl.DefaultUidGenerator;
+import com.github.wujun234.uid.impl.UidDatasourceProperties;
 import com.github.wujun234.uid.impl.UidProperties;
 import com.github.wujun234.uid.worker.DisposableWorkerIdAssigner;
 import com.github.wujun234.uid.worker.WorkerIdAssigner;
-import com.github.wujun234.uid.impl.DefaultUidGenerator;
+import com.zaxxer.hikari.HikariDataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
@@ -14,6 +19,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 
+import javax.sql.DataSource;
+
 /**
  * UID 的自动配置
  *
@@ -21,31 +28,55 @@ import org.springframework.context.annotation.Lazy;
  * @date 2019.02.20 10:57
  */
 @Configuration
-@ConditionalOnClass({ DefaultUidGenerator.class, CachedUidGenerator.class })
-@MapperScan({ "com.github.wujun234.uid.worker.dao" })
-@EnableConfigurationProperties(UidProperties.class)
+@ConditionalOnClass({DefaultUidGenerator.class, CachedUidGenerator.class})
+@MapperScan(value = {"com.github.wujun234.uid.worker.dao"}, sqlSessionFactoryRef = "sqlSessionFactoryUid")
+@EnableConfigurationProperties({UidProperties.class, UidDatasourceProperties.class})
 public class UidAutoConfigure {
 
-	@Autowired
-	private UidProperties uidProperties;
+    @Autowired
+    private UidProperties uidProperties;
+    @Autowired
+    private UidDatasourceProperties datasourceProperties;
 
-	@Bean
-	@ConditionalOnMissingBean
-	@Lazy
-	DefaultUidGenerator defaultUidGenerator() {
-		return new DefaultUidGenerator(uidProperties);
-	}
+    @Bean
+    @ConditionalOnMissingBean
+    @Lazy
+    DefaultUidGenerator defaultUidGenerator() {
+        return new DefaultUidGenerator(uidProperties);
+    }
 
-	@Bean
-	@ConditionalOnMissingBean
-	@Lazy
-	CachedUidGenerator cachedUidGenerator() {
-		return new CachedUidGenerator(uidProperties);
-	}
+    @Bean
+    @ConditionalOnMissingBean
+    @Lazy
+    CachedUidGenerator cachedUidGenerator() {
+        return new CachedUidGenerator(uidProperties);
+    }
 
-	@Bean
-	@ConditionalOnMissingBean
+    @Bean
+    @ConditionalOnMissingBean
     WorkerIdAssigner workerIdAssigner() {
-		return new DisposableWorkerIdAssigner();
-	}
+        return new DisposableWorkerIdAssigner();
+    }
+
+    @Bean(name = "uidDatasource")
+    public DataSource uidDatasource() {
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setDriverClassName(datasourceProperties.getDriverClassName());
+        dataSource.setUsername(datasourceProperties.getUsername());
+        dataSource.setPassword(datasourceProperties.getPassword());
+        dataSource.setJdbcUrl(datasourceProperties.getJdbcUrl());
+        return dataSource;
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactoryUid() throws Exception {
+        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+        factoryBean.setDataSource(uidDatasource());
+        return factoryBean.getObject();
+    }
+
+    @Bean
+    public SqlSessionTemplate sqlSessionTemplateUid() throws Exception {
+        return new SqlSessionTemplate(sqlSessionFactoryUid());
+    }
 }
---
+++ src/main/java/com/github/wujun234/uid/impl/UidDatasourceProperties.java
@@ -0,0 +1,43 @@
+package com.github.wujun234.uid.impl;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "uid-datasource")
+public class UidDatasourceProperties {
+    private String driverClassName = "com.mysql.cj.jdbc.Driver";
+    private String jdbcUrl;
+    private String username;
+    private String password;
+
+    public String getDriverClassName() {
+        return driverClassName;
+    }
+
+    public void setDriverClassName(String driverClassName) {
+        this.driverClassName = driverClassName;
+    }
+
+    public String getJdbcUrl() {
+        return jdbcUrl;
+    }
+
+    public void setJdbcUrl(String jdbcUrl) {
+        this.jdbcUrl = jdbcUrl;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
